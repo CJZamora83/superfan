7e50a3e757572f561b09f539f05ed27ca2442010
Christian Zamora <christian.j.zamora@gmail.com>
2016-09-09 15:25:06 -0700

    run updates

--- app/helpers/instagram_helper.js
+++ app/helpers/instagram_helper.js
@@ -6,7 +6,7 @@ function grabMedia(req, res, next) {
   request.get(`https://api.instagram.com/v1/users/{user-id}/media/recent/?access_token=ACCESS-TOKEN=${req.user.accessToken}`, function(err, response, body) {
     var userData = JSON.parse(body);
     console.log("Grabbed media!");
-    var likedImgLoc = userData.data.map(imgData => [imgData.images.standard_resolution.url, imgData.user.full_name]);
+    var likedImgLoc = userData(imgData => [imgData.images.standard_resolution.url, imgData.user.full_name]);
     res.json(likedImgLoc);
   });
 }
