e63789ecdbf2776d181d26f6e9f107176b020866
alschofield93 <alschofield93@gmail.com>
2016-12-18 22:18:04 -0500

    stuff

--- app/config/routes.js
+++ app/config/routes.js
@@ -29,10 +29,10 @@ router.post('/auth/instagram', authController.instagram);
 router.get('/auth/twitter', twitterController.oauth);
 
 // twitter jwt path (app only auth)
-router.get('/jwt/twitter', twitterController.jwt);
+// router.get('/jwt/twitter', twitterController.jwt);
 
 // twitter invalidate jwt path (only use if app only token becomes compromised for some reason)
-router.get('/invaljwt/twitter', twitterController.invalJwt);
+// router.get('/invaljwt/twitter', twitterController.invalJwt);
 
 // twitter search path
 router.get('/search/twitter/:keyword', twitterController.search);
