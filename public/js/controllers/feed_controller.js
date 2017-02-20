6762f68530cf2382a583eea6428d75c8e55fdd9e
alschofield93 <alschofield93@gmail.com>
2017-02-19 20:29:32 -0500

    better masonry directive

--- public/js/controllers/feed_controller.js
+++ public/js/controllers/feed_controller.js
@@ -8,7 +8,22 @@
 
   function FeedController($scope, $http, $location, twitterDataService, feedService) {
     var vm = this;
-    console.log(feedService.feed);
     $scope.feed = feedService.feed;
+    $scope.brickLimit = 50;
+
+    console.log($scope.feed);
+    // $(window).scroll(function() {
+    //   console.log('scroll height')
+    //   console.log($(window).scrollTop())
+    //   console.log('page scrolled')
+    //   console.log(($(document).height() - $(window).height()) - 300);
+    //   console.log('brick limit')
+    //   console.log($scope.brickLimit);
+    //   if($(window).scrollTop() > (($(document).height() - $(window).height()) - 300)) {
+    //     // ajax call get data from server and append to the div
+    //     console.log('hit');
+    //     $scope.brickLimit += 30;
+    //   }
+    // })
   }
 })();
