47ba8a83ddf22dcf99e16dc3128f0ab048536c56
alschofield93 <alschofield93@gmail.com>
2017-12-16 17:10:32 -0500

    pass celeb image to frontend

--- app/models/celebrity.js
+++ app/models/celebrity.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 
 var celebritySchema = new mongoose.Schema({
   system: String,
+  pretty: String,
   twitter: String,
   instagram: String,
   youtube: Array,
-  pretty: String,
   label: String,
   image: String
 });
